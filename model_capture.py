e6a5c0b4a912993e4811b44c44cc9b0c80cd0b7c
me <mnopqr96@gmail.com>
2015-10-13 14:44:54 +0900

    add files

---
+++ model_capture.py
@@ -0,0 +1,131 @@
+# TODO:
+# Cinema4D自体を、強制終了しないで停止できる
+# try catchで、エラー時に、カメラ位置を初期状態に戻す
+# カメラ取得後の、camera object type check
+# SaveFolderOpenを、Windows対応する
+#
+# Usage:
+# 前準備として、ターゲットカメラを作成し、「main_target_camera」と名前をつけ、ターゲットカメラのターゲット設定を正しく行っておく
+#
+# another way
+# 複数のカメラアングルを一度にレンダリング
+# https://www.youtube.com/watch?v=9AHS5y6avSg
+
+import pprint, os, errno, subprocess, math, time
+import c4d
+from c4d import utils, gui, storage, bitmaps, documents
+
+DEGREE_MIN                = 0
+DEGREE_MAX                = 360
+DEGREE_UNIT               = 60
+SHOW_RENDER_IMG_DIALOG    = True
+SAVE_FILE_PREFIX          = 'frame_'
+SAVE_FILE_ZERO_FILL_DIGIT = 3
+SAVE_FILE_TYPE            = c4d.FILTER_JPG
+
+def main():
+
+    doc             = c4d.documents.GetActiveDocument()
+    rdata           = doc.GetActiveRenderData()
+    base_draw       = doc.GetActiveBaseDraw()
+    img_save_dir    = ''
+
+    # console clear
+    c4d.CallCommand(13957)
+
+    if SAVE_FILE_TYPE == c4d.FILTER_JPG:
+        pass
+    elif SAVE_FILE_TYPE == c4d.FILTER_PNG:
+        pass
+    else:
+        c4d.gui.MessageDialog('save file type not supported')
+        return
+
+    main_target_cam = doc.SearchObject('main_target_camera')
+    
+    if not main_target_cam:
+        c4d.gui.MessageDialog('"main_target_camera" not found. please create one')
+        return
+
+    base_draw.SetSceneCamera(main_target_cam)
+
+    if not img_save_dir:
+        img_save_dir = storage.LoadDialog(c4d.FILESELECTTYPE_ANYTHING, 'Select Image Save Folder', c4d.FILESELECT_DIRECTORY, '', c4d.DOCUMENT_FILEPATH)
+        if not img_save_dir:
+            print 'image save directory select canceled'
+            return
+    
+    if not os.path.isdir(img_save_dir):
+        print 'image save directory not exists'
+        return
+
+    rd   = rdata.GetData()
+    xres = int(round(rd[c4d.RDATA_XRES]))
+    yres = int(round(rd[c4d.RDATA_YRES]))
+    
+    init_mat = main_target_cam.GetMg()
+
+    # print 'init matrix : ' + format(init_mat)
+
+    print '[render start]'
+    
+    degree = DEGREE_MIN
+    while degree < DEGREE_MAX:
+
+        cur_time = time.clock()
+
+        if degree == DEGREE_MIN:
+            h_degree = 0
+        else:
+            h_degree = DEGREE_UNIT
+
+        m       = main_target_cam.GetMg()
+        hpb     = c4d.Vector(utils.Rad(h_degree), utils.Rad(0), utils.Rad(0))
+        rot_mat = c4d.utils.HPBToMatrix(hpb)
+        new_mat = m.__rmul__(rot_mat)
+        
+        main_target_cam.SetMg(new_mat)
+        
+        c4d.EventAdd()
+        
+        bmp = bitmaps.BaseBitmap()
+        bmp.Init(xres, yres, depth = 32)
+
+        res = documents.RenderDocument(doc, rd, bmp, c4d.RENDERFLAGS_EXTERNAL)
+
+        if res == c4d.RENDERRESULT_OK:
+            if SAVE_FILE_TYPE == c4d.FILTER_JPG:
+                filename = os.path.join(img_save_dir, SAVE_FILE_PREFIX + str(degree).zfill(SAVE_FILE_ZERO_FILL_DIGIT) + '.jpg')
+                ret      = bmp.Save(filename, c4d.FILTER_JPG, rd, c4d.SAVEBIT_0)
+            elif SAVE_FILE_TYPE == c4d.FILTER_PNG:
+                bmp.AddChannel(1,0)
+                filename = os.path.join(img_save_dir, SAVE_FILE_PREFIX + str(degree).zfill(SAVE_FILE_ZERO_FILL_DIGIT) + '.png')
+                ret      = bmp.Save(filename, c4d.FILTER_JPG, rd, c4d.SAVEBIT_ALPHA)
+            else:
+                c4d.gui.MessageDialog('save file type not supported')
+                return
+
+            if ret == c4d.IMAGERESULT_OK:
+                if SHOW_RENDER_IMG_DIALOG:
+                    bitmaps.ShowBitmap(bmp)
+
+                print filename + ' export success !'
+            else:
+                print filename + ' export failed !'
+                break
+        
+        spend_time = time.clock() - cur_time
+
+        print '> spend time : ' + str(spend_time)
+
+        degree += DEGREE_UNIT
+    
+    main_target_cam.SetMg(init_mat)
+
+    print '[render end]'
+
+    # Save Folder Open
+    subprocess.call(["open", "-R", filename])
+    
+if __name__=='__main__':
+    main()
---
+++ src/js/main.js
@@ -0,0 +1,148 @@
+﻿var mainCanvas, stage;
+var touchFlag = false;
+var touchLastFlag = false;
+
+var startPos = {x:0, y:0};
+var tempPos = {x:0, y:0};
+var tempFrame = 0;
+var maxFrame = 360;
+
+var prePosX = 0;
+
+var queue;
+var imageUrls = [];
+var imageSprite;
+var startFrame;
+
+function init()
+{
+    mainCanvas = document.getElementById('main_canvas');
+    stage = new createjs.Stage(mainCanvas);
+
+    var manifest = [];
+
+    var id;
+    var url;
+    var i = 0;
+    while(i < maxFrame)
+    {
+        id  = 'frame_' + zfill(i, 3);
+        url = '/c4d.model_viewer/frame/' + id + '.jpg';   
+        imageUrls.push(url);
+        manifest.push({src: url, type:"image", id: id});
+        i++;
+    }
+
+    queue = new createjs.LoadQueue(false);
+    queue.setMaxConnections(5);
+    queue.loadManifest(manifest);
+    queue.on("complete",handleComplete, this);
+}
+function zfill(number, length)
+{
+    return (Array(length).join('0') + number).slice(-length);
+}
+function handleComplete(event) 
+{
+    createImageContainer();
+
+    createjs.Ticker.addEventListener("tick", update);
+    mainCanvas.addEventListener("touchstart", touchStart);
+    mainCanvas.addEventListener("touchmove", touchMove);
+    mainCanvas.addEventListener("touchend", touchEnd);
+    mainCanvas.addEventListener("touchcancel", touchEnd);
+}
+function createImageContainer() 
+{
+
+    var images = [];
+    var i      = 0;
+    var length = imageUrls.length;
+    while(i < length)
+    {
+        images.push(queue.getResult(imageUrls[i]));
+        i++;
+    }
+
+    var spriteSheet = new createjs.SpriteSheet({
+        images: images,
+        frames: { width: 800, height: 600, count: maxFrame },
+    });
+
+    imageSprite = new createjs.Sprite(spriteSheet);
+    imageSprite.scaleX = imageSprite.scaleY = 0.5;
+
+    stage.addChild(imageSprite);
+    stage.update();
+}
+
+function touchStart(e)
+{
+    touchFlag = true;
+    touchLastFlag = false;
+    lastSpeedX = 0;
+    startFrame = tempFrame;
+
+    var touchobj = e.changedTouches[0]; 
+    startPos = {x: parseInt(touchobj.clientX), y: parseInt(touchobj.clientY)};
+    e.preventDefault();
+}
+function touchMove(e)
+{
+    var touchobj = e.changedTouches[0]; 
+    tempPos = {x: parseInt(touchobj.clientX), y: parseInt(touchobj.clientY)};
+    e.preventDefault();
+}
+function touchEnd(e)
+{
+    touchFlag = false;
+
+    lastSpeedX = tempPos.x - prePosX;
+    if(Math.abs(lastSpeedX) > 0)
+    {
+        touchLastFlag = true;
+    }
+
+    e.preventDefault();
+}
+function updateFrame()
+{
+    if(tempFrame < 0)
+    {
+        tempFrame = tempFrame + maxFrame;
+    }
+    
+    tempFrame = tempFrame % maxFrame;
+    
+    imageSprite.gotoAndStop(tempFrame);
+    
+    stage.update();
+}
+function update()
+{
+    if(touchFlag)
+    {
+        var offsetX = (tempPos.x - startPos.x);
+    
+        tempFrame = startFrame + offsetX;
+
+        updateFrame();
+
+        prePosX = tempPos.x;
+
+        return false;
+    }
+
+    if(touchLastFlag)
+    {
+       tempFrame = tempFrame + lastSpeedX; 
+       lastSpeedX = parseInt(lastSpeedX * 0.9);
+
+       if(Math.abs(lastSpeedX) < 1)
+       {
+           touchLastFlag = false;
+       }
+
+       updateFrame();
+    }
+}
